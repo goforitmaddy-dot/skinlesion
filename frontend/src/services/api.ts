d4d920649c4d0295bad35a07b3293b6756973d34
uttam <uttamrai5@icloud.com>
2026-06-18 11:37:20 +0530

    remove console.log

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,9 +4,6 @@ export async function analyzeImage(file: File): Promise<PredictionResponse> {
   const formData = new FormData();
 
   formData.append("file", file);
-
-  console.log(process.env.NEXT_PUBLIC_API_URL);
-
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/predict`, {
     method: "POST",
     body: formData,
